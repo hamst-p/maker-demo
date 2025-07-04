5723258ab53ebce2c478f3cf71358df7c03ae91d
hamst-p <dogsaidwoof@gmail.com>
2025-07-04 14:12:29 +1000

    fix: Add 'use client' directive to resolve Next.js 15 SSR error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 import dynamic from 'next/dynamic'
- 
+
 const ImageCollageApp = dynamic(() => import('../components/ImageCollageApp'), { ssr: false })
- 
+
 export default function Home() {
   return <ImageCollageApp />
 }
