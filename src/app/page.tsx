0f761c7093759db828738048bb03d377b9bb968f
hamst-p <dogsaidwoof@gmail.com>
2025-07-04 14:06:48 +1000

    fix: Update page.tsx for Vercel deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import ImageCollageApp from "../components/ImageCollageApp";
-
+import dynamic from 'next/dynamic'
+ 
+const ImageCollageApp = dynamic(() => import('../components/ImageCollageApp'), { ssr: false })
+ 
 export default function Home() {
-  return <ImageCollageApp />;
+  return <ImageCollageApp />
 }
